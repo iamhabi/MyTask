b91e28a88a717aa86da4023d35b3c0bd91f91ec3
iamhabi <iamhabi@pm.me>
2024-03-06 12:14:44 +0900

    separate AddTask file

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,14 +1,13 @@
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -27,44 +26,5 @@ fun App() {
                 items.add(createNewTaskItem(title))
             }
         }
-    }
-}
-
-@Composable
-fun AddTask(
-    onCreateTask: (String) -> Unit
-) {
-    val input = remember { mutableStateOf("") }
-
-    fun createTask(title: String) {
-        if (title == "") {
-            return
-        }
-
-        onCreateTask(title)
-
-        input.value = ""
-    }
-
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        OutlinedTextField(
-            value = input.value,
-            onValueChange = { input.value = it },
-            modifier = Modifier.onKeyUp(Key.Enter, action = { createTask(input.value) }),
-            label = { Text("Input") },
-            singleLine = true
-        )
-        
-        Spacer(modifier = Modifier.width(10.dp))
-        
-        IconButton(
-            onClick = { createTask(input.value) },
-            content = {
-                Icon(
-                    imageVector = Icons.Default.AddCircle,
-                    contentDescription = "Create new task"
-                )
-            }
-        )
     }
 }
