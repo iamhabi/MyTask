0d3834bf6dc8a91352962f8db765f7b4ab58cc04
iamhabi <iamhabi@pm.me>
2024-03-07 09:30:46 +0900

    change layout by window size

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,6 +1,10 @@
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -10,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import group.AddGroup
 import group.TaskGroup
-import group.TaskGroupList
 import group.createNewTaskGroup
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import task.AddTask
@@ -23,46 +26,63 @@ fun App() {
         val taskGroups = remember { mutableStateListOf<TaskGroup>() }
         val selectedIndex = remember { mutableStateOf(-1) }
         
-        Row(Modifier.fillMaxWidth().padding(16.dp)) {
-            Column(
-                modifier = Modifier.weight(3F),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Box(modifier = Modifier.weight(1F)) {
-                    TaskGroupList(
-                        taskGroups = taskGroups,
-                        selectedIndex = selectedIndex
-                    )
-                }
+        BoxWithConstraints(Modifier.fillMaxWidth().padding(16.dp)) {
+            val isLarge = maxWidth >= 600.dp
 
-                AddGroup { title ->
-                    taskGroups.add(createNewTaskGroup(title))
-                }
-            }
+            Row {
+                if (isLarge) {
+                    Column(
+                        modifier = Modifier.weight(3.5F),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Box(modifier = Modifier.weight(1F)) {
+                            TaskGroupList(
+                                taskGroups = taskGroups,
+                                selectedIndex = selectedIndex
+                            )
+                        }
 
-            Box(
-                modifier = Modifier.weight(1F),
-                contentAlignment = Alignment.Center
-            ) {
-                Divider(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .width(1.dp)
-                )
-            }
+                        AddGroup { title ->
+                            taskGroups.add(createNewTaskGroup(title))
+                        }
+                    }
 
-            Column(
-                modifier = Modifier.weight(7F),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Box(modifier = Modifier.weight(1F)) {
-                    if (selectedIndex.value != -1) {
-                        TaskList(taskGroups[selectedIndex.value].items)
+                    Box(
+                        modifier = Modifier.weight(0.5F),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Divider(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .width(1.dp)
+                        )
                     }
                 }
 
-                AddTask { title ->
-                    taskGroups[selectedIndex.value].items.add(createNewTaskItem(title))
+                Column(
+                    modifier = Modifier.weight(7F)
+                ) {
+                    if (!isLarge) {
+                        IconButton(
+                            onClick = {},
+                            content = {
+                                Icon(
+                                    imageVector = Icons.Default.Menu,
+                                    contentDescription = "Open group"
+                                )
+                            }
+                        )
+                    }
+
+                    Box(modifier = Modifier.weight(1F)) {
+                        if (selectedIndex.value != -1) {
+                            TaskList(taskGroups[selectedIndex.value].items)
+                        }
+                    }
+
+                    AddTask { title ->
+                        taskGroups[selectedIndex.value].items.add(createNewTaskItem(title))
+                    }
                 }
             }
         }
