f18d67d4c83c9e1c4c55f52902466cdb73ba3172
iamhabi <iamhabi@pm.me>
2024-03-07 10:44:30 +0900

    show group on small view

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,17 +1,23 @@
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import group.AddGroup
 import group.GroupView
 import group.TaskGroup
+import group.createNewTaskGroup
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import task.AddTask
 import task.createNewTaskItem
@@ -19,10 +25,12 @@ import task.createNewTaskItem
 @Composable
 @Preview
 fun App() {
+    val taskGroups = remember { mutableStateListOf<TaskGroup>() }
+    val selectedIndex = remember { mutableStateOf(-1) }
+
+    val showGroup = remember { mutableStateOf(false) }
+
     MaterialTheme {
-        val taskGroups = remember { mutableStateListOf<TaskGroup>() }
-        val selectedIndex = remember { mutableStateOf(-1) }
-        
         BoxWithConstraints(Modifier.fillMaxWidth().padding(16.dp)) {
             val isLarge = maxWidth >= 600.dp
 
@@ -40,7 +48,7 @@ fun App() {
                 ) {
                     if (!isLarge) {
                         IconButton(
-                            onClick = {},
+                            onClick = { showGroup.value = true },
                             content = {
                                 Icon(
                                     imageVector = Icons.Default.Menu,
@@ -52,12 +60,52 @@ fun App() {
 
                     Box(modifier = Modifier.weight(1F)) {
                         if (selectedIndex.value != -1) {
+                            showGroup.value = false
+
                             TaskList(taskGroups[selectedIndex.value].items)
                         }
                     }
 
                     AddTask { title ->
                         taskGroups[selectedIndex.value].items.add(createNewTaskItem(title))
+                    }
+                }
+            }
+        }
+    }
+
+    MaterialTheme {
+        when {
+            showGroup.value -> {
+                Box(
+                    modifier = Modifier
+                        .background(Color.White)
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.End
+                    ) {
+                        IconButton(
+                            onClick = { showGroup.value = false },
+                            content = {
+                                Icon(
+                                    imageVector = Icons.Default.Close,
+                                    contentDescription = "Close group"
+                                )
+                            }
+                        )
+
+                        Box(modifier = Modifier.weight(1F)) {
+                            TaskGroupList(
+                                taskGroups = taskGroups,
+                                selectedIndex = selectedIndex
+                            )
+                        }
+
+                        AddGroup { title ->
+                            taskGroups.add(createNewTaskGroup(title))
+                        }
                     }
                 }
             }
