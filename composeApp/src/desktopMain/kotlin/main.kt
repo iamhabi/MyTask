7d3f5b4e14e8cf6f955128938c52889942f9684a
iamhabi <iamhabi@pm.me>
2024-03-19 12:17:10 +0900

    set window's minimumSize

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,9 +1,11 @@
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.awt.Dimension
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "MyTask") {
+        window.minimumSize = Dimension(400, 600)
+
         App()
     }
 }
