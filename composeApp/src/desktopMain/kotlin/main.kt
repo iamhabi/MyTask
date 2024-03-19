93a96ce0ffe47a9bfc1256fe3a77cd4695eedf7f
iamhabi <iamhabi@pm.me>
2024-03-19 15:14:10 +0900

    set minimum window size using dp

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,10 +1,20 @@
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import java.awt.Dimension
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "MyTask") {
-        window.minimumSize = Dimension(400, 600)
+        with (LocalDensity.current) {
+            val minSize = DpSize(200.dp, 300.dp).toSize()
+
+            val minWidth = minSize.width.toInt()
+            val minHeight = minSize.height.toInt()
+
+            window.minimumSize = Dimension(minWidth, minHeight)
+        }
 
         App()
     }
